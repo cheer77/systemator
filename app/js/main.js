82346568d972ddc24c8b759be34ef61cb63afab8
Aleksandr <sheph@ukr.net>
2021-10-14 22:22:21 +0300

    added a slider section

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,18 @@ $(function () {
 
     $('.select-style, .nav__select').styler();
 
+    var swiper = new Swiper(".win__slider", {
+        slidesPerView: 2,
+        spaceBetween: 21,
+        centeredSlidesBounds: true,
+        navigation: {
+            nextEl: ".win__next",
+            prevEl: ".win__prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 
 });
