cd3f1ccf673e2db21d1580a8aea5f354d5a9d252
Dennis <1044442@gmail.com>
2021-10-24 23:21:42 +0300

    blog-page ready

--- app/js/main.js
+++ app/js/main.js
@@ -199,6 +199,50 @@ $(function () {
             })
         }
 
+        function blogSort() {
+            const sortBtns = document.querySelectorAll('.blog-settings__btn--js');
+
+            function handlerActiveBtn(sortBtn) {
+                const sortBtns = sortBtn.parentElement.children;
+
+                for (let i = 0; i < sortBtns.length; i++) {
+                    if (sortBtns[i].classList.contains('active')) {
+                        sortBtns[i].classList.remove('active');
+                    }
+                }
+            }
+
+            function sortItems(sortBtn) {
+                const btnAttr = sortBtn.getAttribute('data-target');
+                const portfolioItems = document.querySelectorAll('.blog-item--js');
+
+                portfolioItems.forEach(item => {
+                    const itemData = item.getAttribute('data-content');
+
+                    if (btnAttr === itemData) {
+                        item.classList.add('sort-show--js');
+                        item.classList.remove('sort-hide--js');
+                    } else {
+                        item.classList.add('sort-hide--js');
+                        item.classList.remove('sort-show--js');
+                    }
+                })
+            }
+
+            sortBtns.forEach(btn => {
+                btn.addEventListener('click', (e) => {
+
+                    handlerActiveBtn(btn);
+
+                    btn.classList.add('active');
+
+                    sortItems(btn);
+                })
+            })
+        }
+
+        blogSort();
+
     }
     catch(e) {
         console.log(e)
